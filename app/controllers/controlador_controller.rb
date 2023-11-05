1230fb273b0ebba9cc2145f30076a1a9967d62f1
Isaac <maripanadillo@gmail.com>
2023-11-05 16:13:46 -0600

    incorporacion de los detalles de la bebida

--- app/controllers/controlador_controller.rb
+++ app/controllers/controlador_controller.rb
@@ -1,7 +1,7 @@
 class ControladorController < ApplicationController
     require 'net/http'
     def index
-        url = URI.parse('https://www.thecocktaildb.com/api/json/v1/1/list.php?c=list')
+        url = URI.parse('https://www.thecocktaildb.com/api/json/v1/1/list.php?c=list&l=es')
         http = Net::HTTP.new(url.host, url.port)
         http.use_ssl = (url.scheme == 'https')
     
@@ -31,14 +31,29 @@ class ControladorController < ApplicationController
 
         if response.code == '200'
             @dat = JSON.parse(response.body)
-            @cat = @dat['drinks'].map { |drink| [drink['strDrink'], drink['strDrinkThumb']] }
+            @cat = @dat['drinks'].map { |drink| [drink['strDrink'], drink['strDrinkThumb'], drink['idDrink']] }
         else
             # Manejar errores de solicitud aquí si es necesario
             puts "Error al obtener datos. Código de respuesta: #{response.code}"
         end
     end
 
-    def detalles
-        
+    def detalles 
+        id_t = params[:id_trago].to_s  
+        idioma= "l=es"
+        url = URI.parse("https://www.thecocktaildb.com/api/json/v1/1/lookup.php?#{idioma}&i=#{id_t}")
+        http = Net::HTTP.new(url.host, url.port)
+        http.use_ssl = (url.scheme == 'https')
+      
+        request = Net::HTTP::Get.new(url.request_uri)
+      
+        response = http.request(request)
+      
+        if response.code == '200'
+            @detalle_trago = JSON.parse(response.body)
+        else
+            # Manejar errores de solicitud aquí si es necesario
+            puts "Error al obtener datos. Código de respuesta: #{response.code}"
+        end
     end
 end
