435b25dbe40832f688f3aa456071a140756b9c96
yasmir <yasmirvargas150302@gmail.com>
2023-11-08 17:28:35 -0600

    cuarto terminado

--- spec/requests/detalles_spec.rb
+++ spec/requests/detalles_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe "ControladorControllers", type: :request do
   it "Probando metodos de controlador" do
-    get "/detalles", params: {id_trago:}
+    get "/detalles", params: {id_trago:"11007"}
     expect(response).to have_http_status(200)  
   end
 end
